761fa254059cf7b0a672d68cbc463f6e69313842
ctarx <ctarx@protonmail.com>
2024-10-04 16:56:02 +0100

    Complete exercise and pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, count) {
+  let result = "";
+  if (count < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < count; i++) {
+    result += word;
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
