898c307788da82497880be6f343acffa96097292
ctarx <ctarx@protonmail.com>
2024-10-06 21:44:45 +0100

    Complete exercise 05 and pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    // (typeof num1 != "number" && typeof num2 != "number") ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
+  // destructuring
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
+  let sum = 0;
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
