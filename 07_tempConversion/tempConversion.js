9886ce5d1d6d529610af45ee2666f0771baf17c5
ctarx <ctarx@protonmail.com>
2024-10-09 19:54:32 +0100

    Complete exercise 07 and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const cTemp = (5 / 9) * (temp - 32);
+  return parseFloat(cTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const fTemp = (9 / 5) * temp + 32;
+  return parseFloat(fTemp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
